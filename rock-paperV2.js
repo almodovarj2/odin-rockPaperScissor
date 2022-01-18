49b5b85b1cb86461858c77db4a1c627f5407762f
almodovarj2 <joshjalmo@gmail.com>
2022-01-17 20:31:48 -0600

    completed v2

--- rock-paperV2.js
+++ rock-paperV2.js
@@ -10,11 +10,25 @@ if (playGame) {
         let computer = computerChoice === 1? "rock"
         : computerChoice === 2? "paper"
         : "scissors";
+
+    let result =
+    playerOne === computer
+    ? "Tie game!"
+    : playerOne === "rock" && computer === "paper"
+    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+    : playerOne === "paper" && computer === "scissors"
+    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+    : playerOne === "scissors" && computer === "rock"
+    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`  
+    : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`
+    alert(result);
+    let playAgain = confirm("Do you want to play again?");
+    playAgain ? location.reload() : alert("Ok. Thanks for playing!");   
     } else {
          alert("You didn't enter rock, paper, or scissors.");
     }
 } else{
-        alert("I guess you changed your mind :(");
+    alert("I guess you changed your mind :(");
     }
 } else {
     alert("Ok, maybe next time.");
